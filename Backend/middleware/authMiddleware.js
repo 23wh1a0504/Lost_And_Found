809c4f51639f5d5180665e476d6248246eb949a5
23wh1a0504 <150162173+23wh1a0504@users.noreply.github.com>
2026-05-24 14:52:44 +0530

    Update authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -3,12 +3,9 @@ const jwt = require("jsonwebtoken");
 const auth = (req,res,next)=>{
   try {
     const token = req.headers.authorization?.replace(/^Bearer\s+/i,"");
-
     if(!token) return res.status(401).json({message:"Authentication required"});
-
     const decoded = jwt.verify(token,process.env.JWT_SECRET);
     req.user = decoded;
-
     next();
   } catch (error) {
     return res.status(401).json({message:"Invalid token"});
